2579119d7c45241313c30f3446e23d5b1bdcea30
I'm Basant <basantnath00@gmail.com>
2024-12-05 01:15:41 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const session = require("express-session");
 const path = require("path");
+
 const PORT = 8000;
 const app = express();
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
 // Middleware to restrict access to /home
-let userInfo = null;
 app.use("/home", (req, res, next) => {
-  if (!userInfo || userInfo.age < 13) {
+  if (!req.session.userInfo || req.session.userInfo.age < 13) {
     return res.redirect("/restricted");
   }
   next();
 });
+
 app.post("/submit", (req, res) => {
   const { name, age, source } = req.body;
 
   if (name && age && source) {
-    userInfo = { name, age, source };
+    req.session.userInfo = { name, age, source };
     return res.redirect("/home");
   } else {
     res.redirect("/");

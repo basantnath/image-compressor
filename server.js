8c48f5c9ad5206780c118fd7ab5521eea1573591
basantnath <basantnath00@gmail.com>
2024-12-05 17:00:48 +0545

    added express-session middleware

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const session = require("express-session");
 const bodyParser = require("body-parser");
 const path = require("path");
 const PORT = 8000;
@@ -7,9 +8,17 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  session({
+    secret: "1011",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 // Middleware to restrict access to /home
-let userInfo = null;
 app.use("/home", (req, res, next) => {
+  const userInfo = req.session.userInfo;
   if (!userInfo || userInfo.age < 13) {
     return res.redirect("/restricted");
   }
@@ -19,7 +28,7 @@ app.post("/submit", (req, res) => {
   const { name, age, source } = req.body;
 
   if (name && age && source) {
-    userInfo = { name, age, source };
+    req.session.userInfo = { name, age, source };
     return res.redirect("/home");
   } else {
     res.redirect("/");
@@ -38,4 +47,4 @@ app.get("/home", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "home.html"));
 });
 
-app.listen(PORT)
+app.listen(PORT);
